c7d18df1edd87bcb5ea19c75eeaaf8e0e59a2891
rafaelfgx <rafaelfgx@gmail.com>
2019-02-27 09:40:49 -0300

    .

---
+++ source/Database/Migrations/20190101000000_Initial.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotNetCoreArchitecture.Database.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "User");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                schema: "User",
+                columns: table => new
+                {
+                    UserId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    Surname = table.Column<string>(maxLength: 200, nullable: false),
+                    Email = table.Column<string>(maxLength: 300, nullable: false),
+                    Login = table.Column<string>(maxLength: 100, nullable: false),
+                    Password = table.Column<string>(maxLength: 500, nullable: false),
+                    Roles = table.Column<int>(nullable: false),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsersLogs",
+                schema: "User",
+                columns: table => new
+                {
+                    UserLogId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<long>(nullable: false),
+                    LogType = table.Column<int>(nullable: false),
+                    Content = table.Column<string>(maxLength: 8000, nullable: true),
+                    DateTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersLogs", x => x.UserLogId);
+                    table.ForeignKey(
+                        name: "FK_UsersLogs_Users_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "User",
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                schema: "User",
+                table: "Users",
+                columns: new[] { "UserId", "Email", "Login", "Name", "Password", "Roles", "Status", "Surname" },
+                values: new object[] { 1L, "administrator@administrator.com", "1h0ATANFe6x7kMHo1PURE74WI0ayevUwfK/+Ie+IWX/xWrFWngcVUwL/ewryn38EMVMQBFaNo4SaVwgXaBWnDw==", "Administrator", "1h0ATANFe6x7kMHo1PURE74WI0ayevUwfK/+Ie+IWX/xWrFWngcVUwL/ewryn38EMVMQBFaNo4SaVwgXaBWnDw==", 3, 1, "Administrator" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                schema: "User",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Login",
+                schema: "User",
+                table: "Users",
+                column: "Login",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersLogs_UserId",
+                schema: "User",
+                table: "UsersLogs",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UsersLogs",
+                schema: "User");
+
+            migrationBuilder.DropTable(
+                name: "Users",
+                schema: "User");
+        }
+    }
+}
